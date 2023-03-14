7fc00fe007f5df5f5eaa5321555f4c4cd0bbf1e7
Yevhenii <boiko.yevgenii@gmail.com>
2023-03-14 18:03:04 +0200

    updates Gallery

--- src/components/ImageGallery/ImagesList.js
+++ src/components/ImageGallery/ImagesList.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import { ImagesGallery } from './ImagesGallery.styled';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageItem';
 
-class ImageGallery extends Component {
-  render() {
-    const { items } = this.props;
-
-    return (
-      <div>
-        <ImagesGallery>
-          {items?.map(photo => (
-            <ImageGalleryItem
-              key={photo.id}
-              src={photo.largeImageURL}
-              alt={photo.id}
-              onClick={this.props.onImageClick}
-            />
-          ))}
-        </ImagesGallery>
-      </div>
-    );
-  }
-}
+const ImageGallery = ({ items, onImageClick }) => {
+  return (
+    <div>
+      <ImagesGallery>
+        {items?.map(photo => (
+          <ImageGalleryItem
+            key={photo.id}
+            src={photo.largeImageURL}
+            alt={photo.id}
+            onClick={onImageClick}
+          />
+        ))}
+      </ImagesGallery>
+    </div>
+  );
+};
 
 export default ImageGallery;
