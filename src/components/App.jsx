4f23240d526d7f2b12c0341731552db819e324f6
Yevhenii <boiko.yevgenii@gmail.com>
2023-03-14 16:13:20 +0200

    modifies App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { GlobalStyle } from 'GlobalStyle';
 import { Layout } from './Layout';
 import { Toaster } from 'react-hot-toast';
+import { getImages } from 'services/ApiService';
+import { BtnContainer } from './Button/Button.styled';
 import ImageGallery from './ImageGallery/ImagesList';
 import Modal from './Modal';
 import SearchBar from './SearchBar';
+import Button from './Button';
+import Spiner from './Loader/Loader';
 
 class App extends Component {
   state = {
     searchValue: '',
+    fotos: [],
+    error: null,
+    status: 'idle',
+    page: 1,
     showModal: false,
     url: {
       srcLarge: '',
@@ -30,11 +38,51 @@ class App extends Component {
   };
 
   handleFormSubmit = searchValue => {
-    this.setState({ searchValue });
+    this.setState({ searchValue, page: 1, fotos: [] });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchValue !== this.state.searchValue) {
+      this.setState({ status: 'pending' });
+
+      getImages(this.state.searchValue.trim(), this.state.page)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ fotos: data.hits, status: 'resolved' });
+        });
+    }
+    if (
+      prevState.searchValue !== this.state.searchValue ||
+      prevState.page !== this.state.page
+    ) {
+      getImages(this.state.searchValue.trim(), this.state.page)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            fotos: [...this.state.fotos, ...data.hits],
+            status: 'resolved',
+          });
+        });
+    }
+  }
+
+  loadMoreImages = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
 
   render() {
-    const { searchValue, page, showModal, url } = this.state;
+    const { searchValue, page, showModal, url, error, fotos, status } =
+      this.state;
+
+    if (status === 'pending') {
+      return <Spiner />;
+    }
+
+    if (status === 'rejected') {
+      return <h2>{error}</h2>;
+    }
 
     return (
       <Layout>
@@ -47,10 +95,16 @@ class App extends Component {
         />
         <SearchBar onSubmit={this.handleFormSubmit} />
         <ImageGallery
+          items={this.state.fotos}
           searchValue={searchValue}
           page={page}
           onImageClick={this.toggleModal}
         />
+        <BtnContainer>
+          {fotos.length > 0 && status === 'resolved' && (
+            <Button onLoadMoreClick={this.loadMoreImages} />
+          )}
+        </BtnContainer>
 
         {showModal && (
           <Modal
