a58cbd4fff1dd4eefbf1eb756f6bbd445a79e952
Yevhenii <boiko.yevgenii@gmail.com>
2023-03-15 23:34:34 +0200

    updates app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { GlobalStyle } from 'GlobalStyle';
 import { Layout } from './Layout';
-import { Toaster } from 'react-hot-toast';
+import { Toaster, toast } from 'react-hot-toast';
 import { getImages } from 'services/ApiService';
 import { BtnContainer } from './Button/Button.styled';
 import ImageGallery from './ImageGallery/ImagesList';
@@ -9,16 +9,68 @@ import Modal from './Modal';
 import SearchBar from './SearchBar';
 import Button from './Button';
 import Spiner from './Loader/Loader';
+import { nanoid } from 'nanoid';
 
 const App = () => {
   const [searchValue, setSearchValue] = useState('');
   const [fotos, setFotos] = useState([]);
-  // const [error, setError] = useState(null);
-  const [status, setStatus] = useState('idle');
-  const [page, setPage] = useState({ currentPage: 1, nextPage: 1 });
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [showBtn, setShowBtn] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [srcLarge, setSrcLarge] = useState('');
   const [altLarge, setAltLarge] = useState('');
+  const [serchId, setSearchId] = useState(null);
+
+  useEffect(() => {
+    if (searchValue === '') {
+      return;
+    }
+
+    setIsLoading(true);
+    getImages(searchValue, page)
+      .then(response => response.json())
+      .then(({ total, totalHits, hits }) => {
+        if (!hits.length) {
+          toast.error(
+            'Sorry, there are no images matching your search query. Please try again',
+            {
+              duration: 3000,
+            }
+          );
+          return;
+        }
+
+        if (page === 1) {
+          toast.success(`Hooray! We found ${totalHits} images.`);
+        }
+
+        setFotos(prevFotos => [...prevFotos, ...hits]);
+        setShowBtn(page < Math.ceil(total / 12));
+
+        if (hits.length < 12 && page !== 1) {
+          return toast.error(
+            "We're sorry, but you've reached the end of search results"
+          );
+        }
+      })
+      .catch(error => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, [page, searchValue, serchId]);
+
+  const loadMoreImages = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  const handleFormSubmit = searchValue => {
+    setSearchValue(searchValue);
+    setSearchId(nanoid());
+    setPage(1);
+    setFotos([]);
+    setShowBtn(false);
+    setError(null);
+  };
 
   const toggleModal = event => {
     if (event !== null) {
@@ -28,50 +80,6 @@ const App = () => {
 
     setShowModal(prevState => !prevState);
   };
-
-  const handleFormSubmit = searchValue => {
-    setSearchValue(searchValue);
-    setPage(1);
-    setFotos([]);
-  };
-
-  useEffect(() => {
-    if (searchValue === '') {
-      return;
-    }
-
-    setStatus('pending');
-
-    if (setSearchValue !== searchValue || setPage !== page) {
-      getImages(searchValue, page)
-        .then(response => response.json())
-        .then(data => {
-          setFotos(data.hits);
-          setStatus('resolved');
-        });
-    }
-
-    // if (setSearchValue !== searchValue || setPage !== page) {
-    //   getImages(searchValue, page)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //       setFotos(prevFotos => [...prevFotos, ...data.hits]);
-    //       setStatus('resolved');
-    //     });
-    // }
-  }, [page, searchValue]);
-
-  const loadMoreImages = () => {
-    setPage(prevState => prevState + 1);
-  };
-
-  if (status === 'pending') {
-    return <Spiner />;
-  }
-
-  // if (status === 'rejected') {
-  //   return <h2>{error}</h2>;
-  // }
 
   return (
     <Layout>
@@ -90,10 +98,10 @@ const App = () => {
         onImageClick={toggleModal}
       />
       <BtnContainer>
-        {fotos.length > 0 && status === 'resolved' && (
-          <Button onLoadMoreClick={loadMoreImages} />
-        )}
+        {showBtn && <Button onLoadMoreClick={loadMoreImages} />}
       </BtnContainer>
+
+      {isLoading && <Spiner />}
 
       {showModal && (
         <Modal
@@ -102,6 +110,8 @@ const App = () => {
           onModalClick={toggleModal}
         />
       )}
+
+      {error && error.message}
     </Layout>
   );
 };
